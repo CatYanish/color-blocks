84c19262cbedeeb4ac273ba673088463f01ae3ae
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-07-18 17:57:40 -0500

    fixed the counter

--- angularclient.js
+++ angularclient.js
@@ -10,11 +10,36 @@ blocks.list = [];
 
 console.log(blocks.list);
 
+blocks.countred = 0;
+blocks.countblue = 0;
+blocks.countyellow = 0;
+blocks.countgreen = 0;
+
+
+
 blocks.color = function(clr) {
 console.log('button clicked, ', clr );
   blocks.list.push(clr);
 
+switch(clr) {
+  case 'red':
+    blocks.countred += 1;
+    break;
+  case 'blue':
+    blocks.countblue += 1;
+    break;
+  case 'yellow':
+    blocks.countyellow += 1;
+    break;
+  case 'green':
+    blocks.countgreen += 1;
+    break;
+}
+
+console.log(blocks.countred);
 }//end blocks.color funciton
 
-console.log(blocks.list);
+
+
+
 }); //end of controller
