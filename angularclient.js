996246584626d21249a1671e1a3d2cbd1bc74a89
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-07-17 19:22:35 -0500

    refactored add color block functionality in angular

--- angularclient.js
+++ angularclient.js
@@ -3,4 +3,18 @@ console.log('js');
 var myApp = angular.module('myApp', []);
 
 myApp.controller('BlocksController', function() {
-  var gallery = this;
+  var blocks = this;
+  console.log('ang sourced');
+
+blocks.list = [];
+
+console.log(blocks.list);
+
+blocks.color = function(clr) {
+console.log('button clicked, ', clr );
+  blocks.list.push(clr);
+
+}//end blocks.color funciton
+
+console.log(blocks.list);
+}); //end of controller
